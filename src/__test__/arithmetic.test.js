16987dffbf11f642fe49e3dec91566a6cd1e77a1
cara ottmar <cara.ottmar@gmail.com>
2020-05-11 14:21:40 -0700

    all tests passing on Strings and Numbers modules.

--- src/__test__/arithmetic.test.js
+++ src/__test__/arithmetic.test.js
@@ -1,5 +1,25 @@
-const sum = require('../lib/arithmetic');
+const arithmetic = require('../lib/arithmetic');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(arithmetic.add(1, 2)).toBe(3);
+});
+
+test('If no numbers passed, return null', () => {
+  expect(arithmetic.add()).toBeNull();
+});
+
+test('If type of parameter passed is not a number, return null', () => {
+  expect(arithmetic.add('A', 'B')).toBeNull();
+})
+
+test('If parameters passed are valid nums, subtract', () => {
+  expect(arithmetic.sub(3, 2)).toBe(1);
+});
+
+test('If paramets passed are not numbers, return null', () => {
+  expect(arithmetic.sub('A', 'B')).toBeNull();
+});
+
+test('If no parameter is passed, return null', () => {
+  expect(arithmetic.sub()).toBeNull();
 });
--- src/lib/arithmetic.js
+++ src/lib/arithmetic.js
@@ -1,5 +1,26 @@
-function sum(a, b) {
+const arithmetic = module.exports = {};
+
+arithmetic.add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number' || !a || !b) {
+    return null;
+  }
   return a + b;
 }
 
-module.exports = sum;
+arithmetic.sub = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number' || !a || !b) {
+    return null;
+  }
+  return a - b;
+}
+
+
+
+console.log(arithmetic.add('A', 'B'));
+console.log(arithmetic.add(1, 2));
+console.log(arithmetic.add());
+
+console.log(arithmetic.sub('A', 'B'));
+console.log(arithmetic.sub(3, 2));
+console.log(arithmetic.sub());
+
